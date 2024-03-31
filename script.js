45b4589387d3070f1cfccfbbec7d9345ea6cef9e
majjeed <abdulmajeed.hersi@gmail.com>
2024-03-31 20:19:52 +0100

    fix button 16 boxes appearing per row

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 let container = document.querySelector("#container");
+createGrid();
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let gridDiv = document.createElement("div");
-        gridDiv.setAttribute("class", "gridDiv");
-        //gridDiv.style.cssText = "border: 2px solid red; width: 75px; height: 75px";
-        container.appendChild(gridDiv);
+function createGrid(num = 16) {
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            let gridDiv = document.createElement("div");
+            gridDiv.setAttribute("class", "gridDiv");
+            //gridDiv.style.cssText = "border: 2px solid red; width: 75px; height: 75px";
+            container.appendChild(gridDiv);
+        }
     }
+    let gridDivs = document.querySelectorAll(".gridDiv");
+    gridDivs.forEach(element => {
+        element.addEventListener("mouseover", function () {
+            this.style.backgroundColor = "green";
+        })
+    });
 }
 
-let gridDivs = document.querySelectorAll(".gridDiv");
+// function getDivs() {
+//     return document.querySelectorAll(".gridDiv");
+// }
 
-gridDivs.forEach(element => {
-    element.addEventListener("mouseover", function() {
-        this.style.backgroundColor = "green";
-    })
-});
+
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", event => {
+    let gridDivs = document.querySelectorAll(".gridDiv");
+    gridDivs.forEach(element => {
+        element.remove();
+    });
+
+    let gridNum = Number(prompt("How many grids do you want?"));
+    if (isNaN(gridNum) || gridNum === 0) {
+        gridNum = 16;
+    } else if (gridNum > 100) {
+        gridNum = 100;
+    }
+    createGrid(gridNum);
+})
