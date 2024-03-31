7123693ad6150c9204f126fa4b4270ca112f17e2
majjeed <abdulmajeed.hersi@gmail.com>
2024-03-31 20:40:59 +0100

    fix grid calc working for x-by-x

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ function createGrid(num = 16) {
             let gridDiv = document.createElement("div");
             gridDiv.setAttribute("class", "gridDiv");
             //gridDiv.style.cssText = "border: 2px solid red; width: 75px; height: 75px";
+            gridDiv.style.cssText = 
+            `
+            border: 2px solid red;
+            width: ${container.offsetWidth/num - 2}px;
+            padding-bottom: ${container.offsetWidth/num - 2}px;
+            `;
             container.appendChild(gridDiv);
         }
     }
